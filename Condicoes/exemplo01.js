d99519a750fc69f2361fa4414b7f0d3b073220dc
Korvo2Tuirow <korvo2irow@live.com>
2021-10-08 14:41:26 -0300

    aula de condições

---
+++ Condicoes/exemplo01.js
@@ -0,0 +1,12 @@
+console.log('O console funcionou corretamente!')
+
+var vel = 60.5
+
+console.log(`a velocidade do seu carro é ${vel} KM/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida`)
+
+}
+
+console.log(`Use o cinto de segurança`)
---
+++ Condicoes/exemplo02.js
@@ -0,0 +1,14 @@
+var país = 'EUA'
+
+console.log(`Vivendo em ${país}`)
+
+if (país == 'Brasil'){
+
+    console.log('Você é Brasileiro')
+
+}
+
+else{
+
+    console.log('Você é Gringo')
+}
