5b27d4b4e36ee801f52ba1520e92c0a6acc5808b
Korvo2Tuirow <korvo2irow@live.com>
2021-10-18 16:25:39 -0300

    aulas

---
+++ while_do_for/while_do_for.js
@@ -0,0 +1,28 @@
+
+// while = enquanto
+var c = 1
+
+while (c <= 6 ){
+
+    console.log(`Tudo bem ${c}`)
+    c++ // c = c + 1
+} 
+
+
+
+// do = faça
+var x = 1
+
+do{
+    console.log(`Passo ${x}`)
+    x++
+
+} while (x <= 6) 
+
+
+
+// For (inicio; teste; incremento ){...}
+//for (var y = 1; y <= 6; y++)
+for (y=1; y<=6; y++){
+    console.log (`Teste ${y}`)
+}
